f4ea2aa3b05e8148bec1a3043aacecce9451e2a6
kishikawa katsumi <kishikawakatsumi@mac.com>
2023-09-20 20:34:44 +0900

    Update pgroona

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM groonga/pgroonga:3.1.3-alpine-14
+
+COPY config/ /var/lib/postgresql/config/
+
+ENV POSTGRES_DB database
+ENV POSTGRES_USER postgres
+ENV POSTGRES_PASSWORD postgres
+
+EXPOSE 10000
+CMD ["docker-entrypoint.sh", "-c", "config_file=/var/lib/postgresql/config/postgresql.conf"]
--- main.ts
+++ main.ts
@@ -5,17 +5,74 @@ const POOL_CONNECTIONS = 4;
 const dbPool = new Pool({
   hostname: "127.0.0.1",
   port: 5432,
-  user: Deno.env.get("POSTGRES_USER"),
-  password: Deno.env.get("POSTGRES_PASSWORD"),
-  database: Deno.env.get("POSTGRES_DB"),
+  user: "postgres",
+  password: "postgres",
+  database: "database",
 }, POOL_CONNECTIONS);
 
 const client = await dbPool.connect();
 
+
+client.queryArray("CREATE EXTENSION IF NOT EXISTS pgroonga;")
+
+const tables = ["macos12", "macos13", "ios15", "ios16"];
+
+for (const table of tables) {
+client.queryArray(
+`CREATE SEQUENCE ${table}_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;`);
+
+client.queryArray(
+`CREATE TABLE ${table} (
+    id integer DEFAULT nextval('${table}_id_seq') PRIMARY KEY NOT NULL,
+    group_id integer NOT NULL,
+    source text NOT NULL,
+    target text NOT NULL,
+    language text NOT NULL,
+    file_name text NOT NULL,
+    bundle_name text NOT NULL,
+    bundle_path text NOT NULL,
+    platform text NOT NULL
+);`);
+
+
+client.queryArray(
+`CREATE INDEX ${table}_bundle_name_index ON ${table} USING btree (bundle_name);
+CREATE INDEX ${table}_bundle_path_index ON ${table} USING btree (bundle_path);
+CREATE INDEX ${table}_file_name_index ON ${table} USING btree (file_name);
+CREATE INDEX ${table}_group_id_index ON ${table} USING btree (group_id);
+CREATE INDEX ${table}_group_id_language_index ON ${table} USING btree (group_id, language);
+CREATE INDEX ${table}_language_group_id_index ON ${table} USING btree (language, group_id);
+CREATE INDEX ${table}_language_index ON ${table} USING btree (language);
+CREATE INDEX ${table}_platform_index ON ${table} USING btree (platform);
+CREATE INDEX ${table}_source_index ON ${table} USING pgroonga (source);
+CREATE INDEX ${table}_target_index ON ${table} USING pgroonga (target);`);
+}
+
+
+for (const table of tables) {
+
 let counter = 0;
 let groupId = 1;
 const groupIds: { [key: string]: number } = {};
-const rootDir = "data/macos";
+const rootDir = {
+  macos12: "data/macos/12.6",
+  macos13: "data/macos/13.5.2",
+  ios15: "data/ios/15.7",
+  ios16: "data/ios/16.6",
+}[table];
+const platform = {
+  macos12: "macOS",
+  macos13: "macOS",
+  ios15: "iOS",
+  ios16: "iOS",
+}[table];
+
 for await (const directory of Deno.readDir(rootDir)) {
   const localizable: Localizable = JSON.parse(
     await Deno.readTextFile(path.join(rootDir, directory.name)),
@@ -39,7 +96,7 @@ for await (const directory of Deno.readDir(rootDir)) {
       }
 
       await client.queryArray(
-        `INSERT INTO macos (group_id, source, target, language, file_name, bundle_name, bundle_path, platform) VALUES($1, $2, $3, $4, $5, $6, $7, $8);`,
+        `INSERT INTO ${table} (group_id, source, target, language, file_name, bundle_name, bundle_path, platform) VALUES($1, $2, $3, $4, $5, $6, $7, $8);`,
         [
           gid,
           key,
@@ -48,7 +105,7 @@ for await (const directory of Deno.readDir(rootDir)) {
           localization.filename,
           localizable.framework,
           localizable.bundlePath,
-          "macOS",
+          platform,
         ],
       );
 
@@ -58,6 +115,8 @@ for await (const directory of Deno.readDir(rootDir)) {
 }
 
 console.log(counter);
+}
+
 client.release();
 
 interface Localizable {
